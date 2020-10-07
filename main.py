1af175b5328667d1ea39924963cf165d3751af6d
Lunnehaxx <stairwaytobalken@gmail.com>
2020-10-07 11:39:11 +0200

    Stared writing class. /CL

---
+++ main.py
@@ -0,0 +1,8 @@
+"Pokemon Battle GO!"
+
+class Poketer():
+    def __init__(self, name, mood, health, attack):
+        self.name = name
+        self.name = mood
+        self.name = health
+        self.name = attack
