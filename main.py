6145ef5f5d0dcaf1f57234ebb0305bd1012ab4b4
Lunnehaxx <stairwaytobalken@gmail.com>
2020-10-07 12:07:58 +0200

    Added object from Poketer class, user_pokemon and cpu_pokemon. Added class User. with code. Added self.team list.

--- main.py
+++ main.py
@@ -3,6 +3,15 @@
 class Poketer():
     def __init__(self, name, mood, health, attack):
         self.name = name
-        self.name = mood
-        self.name = health
-        self.name = attack
+        self.mood = mood
+        self.health = health
+        self.attack = attack
+
+user_pokemon = Poketer("Happy Hasse", "happy", 100, 25)
+cpu_pokemon = Poketer("Aggressive Ada", "angry", 100, 25)
+
+class User():
+    def __init__(self, name, team):
+        self.name = name
+        self.team = team
+self.team = []
