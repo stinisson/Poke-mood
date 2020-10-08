6be2da0f433db37ac70a7766094bfb438352c5bb
sophie.n <Sophie-nhan@hotmail.com>
2020-10-08 13:50:49 +0200

    next

--- main.py
+++ main.py
@@ -44,14 +44,27 @@ def main():
     if city == "Göteborg":
         user_pokemon.health += city_dic["Göteborg"]
         cpu_pokemon.health += city_dic['Stockholm']
-        print(f"Happy Hasse Health incressed by {city_dic['Göteborg']}. Total Health {user_pokemon.health}")
-        print(f"Aggressive Ada Health incressed by {city_dic['Stockholm']}. Total Health {cpu_pokemon.health}")
+        print(f"Happy Hasse Health increased by {city_dic['Göteborg']}. Total Health: {user_pokemon.health}")
+        print(f"Aggressive Ada Health increased by {city_dic['Stockholm']}. Total Health: {cpu_pokemon.health}")
 
     elif city == "Stockholm":
         user_pokemon.health += city_dic["Stockholm"]
         cpu_pokemon.health += city_dic['Göteborg']
-        print(f"Happy Hasse Health incressed by {city_dic['Stockholm']}. Total Health {user_pokemon.health}")
-        print(f"Aggressive Ada Health incressed by {city_dic['Göteborg']}. Total Health {cpu_pokemon.health}")
+        print(f"Happy Hasse Health increased by {city_dic['Stockholm']}. Total Health: {user_pokemon.health}")
+        print(f"Aggressive Ada Health increased by {city_dic['Göteborg']}. Total Health: {cpu_pokemon.health}")
+
+    print("Time to play!")
+    while True:
+        user_choose = input("Do you wanna attack or standby?")
+        if user_choose == "Attack":
+            cpu_pokemon.health -= user_pokemon.attack
+            print(f"Aggressive Ada now has {cpu_pokemon.health} in health")
+        elif user_choose == "Standby":
+            print("You chose to standby.")
+
+    user_pokemon.health -= cpu_pokemon.attack
+    print(f"Aggressive Ada attacked you! Your health is now at {user_pokemon.health}")
+
 
 if __name__ == '__main__':
     main()
