676b5e30afe4658e8b396a0d92942183bdf93ecb
stinisson <krikarlss@gmail.com>
2020-10-08 11:28:51 +0200

    Add method add_team in class User

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
 "Pokemon Battle GO!"
 
-class Poketer():
+
+class Poketer:
     def __init__(self, name, mood, health, attack):
         self.name = name
         self.mood = mood
         self.health = health
         self.attack = attack
 
-user_pokemon = Poketer("Happy Hasse", "happy", 100, 25)
-cpu_pokemon = Poketer("Aggressive Ada", "angry", 100, 25)
 
-class User():
-    def __init__(self, name, team):
+class User:
+    def __init__(self, name, team =""):
         self.name = name
-        self.team = team
-self.team = []
+        self.team = []
+
+    def add_team(self, poketer):
+        self.team.append(poketer)
+
+
+def main():
+    user_pokemon = Poketer("Happy Hasse", "happy", 100, 25)
+    cpu_pokemon = Poketer("Aggressive Ada", "angry", 100, 25)
+    print(f"Du valde {user_pokemon.name}")
+    print(f"Din motståndare valde {cpu_pokemon.name}")
+
+    user = User("Martin")
+    user.add_team(user_pokemon)
+
+
+if __name__ == '__main__':
+    main()
