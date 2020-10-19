3b767bd4b60d45e9bbf0f42de9af81d0dd1c727c
Martin <martin.drugge@telia.com>
2020-10-19 11:23:30 +0200

    print fix

--- main.py
+++ main.py
@@ -22,12 +22,12 @@ class Poketer:
     def attack_fnc(self, opponent_pokemon):
         opponent_pokemon.health -= self.attack
         atk_txt(self.name, opponent_pokemon.name, "3 2 1...")
-        if self.health >= self.max_health / 2:
-                print(f"{self.name} hälsa: {colored(self.health, 'green')}\n")
-        elif self.max_health / 4 <= self.health <= self.max_health / 2:
-                print(f"{self.name} hälsa: {colored(self.health, 'yellow')}\n")
-        elif self.health <= self.max_health / 4:
-                print(f"{self.name} hälsa: {colored(self.health, 'red')}\n")
+        if opponent_pokemon.health >= opponent_pokemon.max_health / 2:
+                print(f"{opponent_pokemon.name} hälsa: {colored(opponent_pokemon.health, 'green')}\n")
+        elif opponent_pokemon.max_health / 4 <= opponent_pokemon.health <= opponent_pokemon.max_health / 2:
+                print(f"{opponent_pokemon.name} hälsa: {colored(opponent_pokemon.health, 'yellow')}\n")
+        elif opponent_pokemon.health <= opponent_pokemon.max_health / 4:
+                print(f"{opponent_pokemon.name} hälsa: {colored(opponent_pokemon.health, 'red')}\n")
 
 
     def block(self, opponent, opponent_pokemon):
@@ -129,7 +129,6 @@ def main():
     while (user_pokemon.health >= 0) and (cpu_pokemon.health >= 0):
         if user_pokemon.health <= 0 or cpu_pokemon.health <= 0:
             if cpu_pokemon.health <= 0:
-
                 break
             if user_pokemon.health <= 0:
                 print(f'*** Din poketer {user_pokemon.name} svimmade. {cpu.name} vann! ***')
@@ -140,17 +139,12 @@ def main():
             user_choose = int(input("Vill du [1] attackera eller [2] blockera? "))
             if user_choose == 1:
                 user_pokemon.attack_fnc(cpu_pokemon)
-
-
-                time.sleep(1)
-                if user_pokemon.health <= 0 or cpu_pokemon.health <= 0:
-                    if cpu_pokemon.health > 0:
-                        print(f'*** Det är {cpu.name} tur.. ***')
-                        cpu_pokemon.attack_fnc(user_pokemon)
-                    elif user_pokemon.health <= 0:
-                        print(f'*** {cpu.name} poketer {cpu_pokemon.name} svimmade. Du vann!! ***')
-                        break
-
+                if cpu_pokemon.health > 0:
+                    print(f'*** Det är {cpu.name} tur.. ***')
+                    cpu_pokemon.attack_fnc(user_pokemon)
+                elif user_pokemon.health <= 0:
+                    print(f'*** {cpu.name} poketer {cpu_pokemon.name} svimmade. Du vann!! ***')
+                    break
             elif user_choose == 2:
                 user_pokemon.block(cpu, cpu_pokemon)
 
